d6328317c86d29d4f4289d325c2f0462fd6c11f6
Ramakrishna Pendem <prkrishna2091@gmail.com>
2022-04-09 11:17:10 +0530

    schedular code created

---
+++ src/main/java/com/prk/springboot_schedular/SpringBootSchedularApplication.java
@@ -0,0 +1,13 @@
+package com.prk.springboot_schedular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSchedularApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSchedularApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/prk/springboot_schedular/SpringBootSchedularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prk.springboot_schedular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSchedularApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
